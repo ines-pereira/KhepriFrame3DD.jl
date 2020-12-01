309ec447d6705966fc4cc145b315dac7435c0914
aptmcl <antonio.m>
2020-12-01 18:37:54 +0000

    Added direct dll access to Frame3DD.

--- src/Frame3DD.jl
+++ src/Frame3DD.jl
@@ -137,7 +137,7 @@ realize(b::FR3DD, s::Panel) =
 
 
 #new_truss_analysis(v=nothing; self_weight=false, backend=frame3dd) =
-displacements_from_frame3dd(b::FR3DD, filename, load) =
+displacements_from_frame3dd(b::FR3DD, filename, load, self_weight=false) =
   let nodes = process_nodes(b.truss_nodes, load),
       bars = process_bars(b.truss_bars, nodes),
       supports = unique(filter(s -> s != false, map(n -> n.family.support, nodes))),
@@ -196,7 +196,7 @@ displacements_from_frame3dd(b::FR3DD, filename, load) =
       println(io, "\t\t%% begin static load case 1 of 1");
       println(io, "%% gravitational acceleration for self-weight loading");
       println(io, "%% gX         gY         gZ");
-      println(io, "  0.0        0.0        0.0\n");
+      println(io, "  0.0        0.0        $(self_weight ? -9.81 : 0.0)\n");
       println(io, "$(length(loaded_nodes))\t\t%% number of loaded nodes");
       println(io, "%% j\t\tFx\t\tFy\t\tFz\t\tMxx\t\tMyy\t\tMzz");
       for n in loaded_nodes
@@ -214,38 +214,185 @@ displacements_from_frame3dd(b::FR3DD, filename, load) =
     end
   end
 
-frame3dd_plugin = joinpath(dirname(dirname(abspath(@__FILE__))), "bin", "frame3dd.exe")
+frame3dd_plugin = joinpath(dirname(abspath(@__DIR__)), "bin", "frame3dd.exe")
 
 frame3dd_simulation_path() =
   mktempdir(tempdir(), prefix="Frame3DD_")
 
-##########################################
-backend_truss_analysis(b::FR3DD, load::Vec) =
+#=
+backend_truss_analysis(b::FR3DD, load::Vec, self_weight::Bool) =
   # Ensure extension is FMM to force Matlab mode
   let simulation_folder = frame3dd_simulation_path(),
-#      input_path = joinpath(simulation_folder, "IOdata.FMM"),
-#      output_path = joinpath(simulation_folder, "IOdata.OUT") ,
-#      output_m_path = joinpath(simulation_folder, "IOdata_out.m")
        input_path = joinpath(simulation_folder, "IOdata.IN"),
        output_path = joinpath(simulation_folder, "IOdata.OUT")
     @info input_path
-    displacements_from_frame3dd(b, input_path, load)
-    try
-      #run(`$frame3dd_plugin -w -i $input_path -o $output_path`)
-      #run(`$frame3dd_plugin -i $input_path -o $output_path`)
+    displacements_from_frame3dd(b, input_path, load, self_weight)
+    #try
       withenv("FRAME3DD_OUTDIR"=>simulation_folder) do
         run(`$frame3dd_plugin -q -i $input_path -o $output_path`)
       end
-    catch e
-    end
+    #catch e
+    #end
     lines = readlines(output_path)
     idx1 = findfirst(l->startswith(l, "N O D E   D I S P L A C E M E N T S"), lines) + 2
     idx2 = findnext(l->startswith(l, "F R A M E   E L E M E N T   E N D   F O R C E S"), lines, idx1)
     Dict([let strs = split(line)
-            parse(Int, strs[1])=>vxyz(parse.(Float64, strs[2:4])...)
+            parse(Int, strs[1])=>parse.(Float64, strs[2:7])
           end
           for line in lines[idx1:idx2-1]])
   end
 
 backend_node_displacement_function(b::FR3DD, results) =
-  n -> get(results, n.id, vx(0))
+  n -> vxyz(get(results, n.id, (0,0,0,0,0,0))[1:3]..., world_cs)
+
+=#
+
+#
+using Libdl
+f3ddlib = dlopen(joinpath(dirname(abspath(@__DIR__)), "bin", "Frame3DDLib"))
+f3ddmain = dlsym(f3ddlib, :simulate)
+
+#dlclose(f3ddlib)
+##########################################
+backend_truss_analysis(be::FR3DD, load::Vec, self_weight::Bool) =
+    let nodes = process_nodes(be.truss_nodes, load),
+        bars = process_bars(be.truss_bars, nodes),
+        supports = unique(filter(s -> s != false, map(n -> n.family.support, nodes))),
+        loaded_nodes = filter(!truss_node_is_supported, nodes),
+        supported_nodes = filter(truss_node_is_supported, nodes),
+        shear = 0,		     # 1: include shear deformation effects, 0: don't
+        geom  = 0,		     # 1: include geometric stiffness effects, 0: don't
+        exagg = 1,		     # exaggeration factor for Gnuplot output
+        scale = 1.0,		   # zoom scale factor for Gnuplot plotting
+        dx = 1.0,		       # x-axis increment for internal force calc's
+        nN = length(nodes), # number of nodes
+        nE = length(bars),  # number of bars
+        nodes_coords = zeros(Float64, 3*(nN+1)), # X,Y,Z node coordinates (global)
+        nodes_radius = zeros(Float32, nN + 1),   # node size radius, for finite sizes
+        DoF = 6*nN,
+        nR = length(supported_nodes),            # number of restrained nodes
+        q = zeros(Int32, DoF+1),
+        r = zeros(Int32, DoF+1),
+        sumR = 0
+        empty!(be.truss_node_data)
+        append!(be.truss_node_data, nodes)
+        empty!(be.truss_bar_data)
+        append!(be.truss_bar_data, bars)
+      for n in nodes
+          i = n.id
+          p = n.loc
+        for j in 1:3
+          nodes_coords[3*i+j]=p.raw[j]
+        end
+      end
+      for n in supported_nodes
+        s = n.family.support
+        j = n.id
+        sj = [Int(s.ux), Int(s.uy), Int(s.uz), Int(s.rx), Int(s.ry), Int(s.rz)]
+        for l in 5:-1:0
+          r[6*j-l+1] = sj[6-l]
+        end
+      end
+      sumR = sum(r)
+      sumR > 4 || error("Un-restrained structure: only $(sumR) imposed reactions. At least 4 reactions are required to support static loads.")
+      sumR < DoF || error("Fully restrained structure: $(sumR) imposed reactions >= $(DoF) degrees of freedom")
+      for i in 1:DoF
+        q[i+1] = r[i+1] > 0 ? 0 : 1
+      end
+      L = zeros(Float64, nE+1)   # length of each element
+      Le = zeros(Float64, nE+1)  # effective length of each element
+      N1  = zeros(Int32, nE+1)   # node #1 of each element
+      N2  = zeros(Int32, nE+1)   # node #2 of each element
+    	Ax  = zeros(Float32, nE+1) # cross section area of each element	*/
+    	Asy = zeros(Float32, nE+1) # shear area in local y direction 	*/
+    	Asz = zeros(Float32, nE+1) # shear area in local z direction	*/
+    	Jx  = zeros(Float32, nE+1) # torsional moment of inertia 		*/
+    	Iy  = zeros(Float32, nE+1) # bending moment of inertia about y-axis */
+    	Iz  = zeros(Float32, nE+1) # bending moment of inertia about z-axis */
+    	E   = zeros(Float32, nE+1) # frame element Young's modulus	*/
+    	G   = zeros(Float32, nE+1) # frame element shear modulus		*/
+    	p   = zeros(Float32, nE+1) # element rotation angle about local x axis */
+    	d   = zeros(Float32, nE+1) # element mass density			*/
+      for bar in bars
+        f = family_ref(be, bar.family)
+        g = f.geometry[]
+        b = bar.id
+        N1[b+1] = bar.node1.id
+        N2[b+1] = bar.node2.id
+        Ax[b+1] = g.Ax
+        Asy[b+1] = g.Asy
+        Asz[b+1] = g.Asz
+        Jx[b+1] = g.Jxx
+        Iy[b+1] = g.Iyy
+        Iz[b+1] = g.Izz
+        E[b+1] = f.E
+        G[b+1] = f.G
+        p[b+1] = f.p
+        d[b+1] = f.d
+        n1 = N1[b+1]
+        n2 = N2[b+1]
+        L[b+1] = distance(bar.node2.loc, bar.node1.loc)
+        Le[b+1] = L[b+1] - nodes_radius[n2+1] - nodes_radius[n1+1]
+      end
+      nF = length(loaded_nodes)
+      F_mech = zeros(Float64, DoF+1)
+      for n in loaded_nodes
+        F_mech[6*n.id - 5 + 1] = load.x;
+        F_mech[6*n.id - 4 + 1] = load.y;
+        F_mech[6*n.id - 3 + 1] = load.z;
+      end
+      gX = [0f0, 0f0]
+      gY = [0f0, 0f0]
+      gZ = [0f0, self_weight ? -9.81f0 : 0f0]
+      nFs = Int32[0, nF]
+      F_mechs = [Ref(F_mech, 1), Ref(F_mech, 1)]
+      D = zeros(DoF+1)	  # displacements of each node
+      ccall(f3ddmain, Int32, (
+            Int32,        # number of Nodes
+            Int32,        # number of frame Elements
+            Ref{Float64}, # X,Y,Z node coordinates (global)
+            Ref{Float32}, # node size radius, for finite sizes
+            Int32,        # number of restrained nodes
+            Ref{Int32},   # reaction data
+            Ref{Int32},   # reaction data
+            Int32,        # total no. of reactions
+            Ref{Float64},	# length of each element
+            Ref{Float64},	# effective length of each element
+            Ref{Int32},   # node #1 of each element
+            Ref{Int32},   # N2,    // node #2 of each element
+            Ref{Float32},	# cross section area of each element
+            Ref{Float32},	# shear area in local y direction 	*/
+            Ref{Float32},	# shear area in local z direction	*/
+            Ref{Float32},	# torsional moment of inertia 		*/
+            Ref{Float32},	# bending moment of inertia about y-axis */
+            Ref{Float32},	# bending moment of inertia about z-axis */
+            Ref{Float32},	# frame element Young's modulus	*/
+            Ref{Float32},	# frame element shear modulus		*/
+            Ref{Float32},	# element rotation angle about local x axis */
+            Ref{Float32},	# element mass density			*/
+            Int32,        # number of Load cases
+            Int32,        # number of Load cases
+            Ref{Float32}, # gravitational acceleration in global X
+            Ref{Float32}, # gravitational acceleration in global Y
+            Ref{Float32}, # gravitational acceleration in global Z
+            Ref{Int32},   # number of loaded nodes
+            Ref{Ptr{Float64}},  # mechanical load vectors, all load cases
+            # RESULTS
+            Ref{Float64}  # displacements of each node
+            ),
+        nN, nE,
+        nodes_coords, nodes_radius,
+        nR, q, r, sumR,
+        L, Le, N1, N2, Ax, Asy, Asz, Jx, Iy, Iz, E, G, p, d,
+        1, 0,
+        gX, gY, gZ,
+        nFs,
+        F_mechs,
+        # RESULTS
+        D)
+      D
+    end
+#  end
+
+backend_node_displacement_function(b::FR3DD, results) =
+  n -> vxyz(results[6*n.id-4:6*n.id-2]..., world_cs)
